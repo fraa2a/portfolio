6f07df32135b87341587e32f65e2677074f83488
unknown <123754424+fraa2a@users.noreply.github.com>
2026-06-23 11:44:58 +0200

    added simple page contents

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import './style.css'
-import heroImg from './assets/hero.png'
+import 'iconify-icon'
 
-const button = document.getElementById("toggle-theme");
+const themeButton = document.getElementById("toggle-theme");
+const savedTheme = localStorage.getItem("theme");
+const root: HTMLElement = document.documentElement;
 
-button!.onclick = function() {
-    const root: HTMLElement = document.documentElement;
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (savedTheme === "dark" || (!savedTheme && prefersDark)) {
+  root.dataset.theme = "dark";
+}
+
+
+themeButton!.onclick = function() {
+    
     if (root.dataset.theme == "dark") {
         root.dataset.theme = "light";
+        localStorage.setItem("theme", "light");
     } else {
         root.dataset.theme = "dark";
+        localStorage.setItem("theme", "dark");
     }
 }
